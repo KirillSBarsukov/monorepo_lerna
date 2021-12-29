4896c949a0c45f30c7c291ec8d2a757c9eece408
Kirill Barsukov <kirillsbarsukov@gmail.com>
2021-12-29 16:07:21 -0500

    commit

--- packages/core/src/index.ts
+++
@@ -1,7 +0,0 @@
-import { Alpha } from "@kirillsbarsukov/alpha";
-import { Beta } from "@kirillsbarsukov/beta";
-import { Gamma } from "@kirillsbarsukov/gamma";
-
-console.log(new Alpha());
-console.log(new Beta());
-console.log(new Gamma());
